49263c298e542d3451d1f7f0aae2035b5deec14a
Your Name <you@example.com>
2023-04-20 07:36:59 -0700

    modified 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 
-int main(void) {
-  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, message, sizeof(message) - 1);
-  return 1;
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (indicating error)
+ */
+int main(void)
+{
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return (1);
 }
+
