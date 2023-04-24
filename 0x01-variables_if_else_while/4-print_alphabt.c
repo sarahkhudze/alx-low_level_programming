6c5d3a15590beb1d8ef621b43019358989d14ec4
Your Name <you@example.com>
2023-04-24 06:59:39 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c = 'a';
+
+    while (c <= 'z') {
+        if (c != 'e' && c != 'q') {
+            putchar(c);
+        }
+        c++;
+    }
+    
+    putchar('\n');
+
+    return 0;
+}
